871c3f39f1c27955fa36bf433f499d11e5a21013
tu ti tu <rapa@example.com>
2018-05-20 18:21:06 +0300

    changes

--- src/main/java/com/eatstime/eatstimesupport/domain/Course.java
+++ src/main/java/com/eatstime/eatstimesupport/domain/Course.java
@@ -1,30 +1,30 @@
-package com.eatstime.eatstimesupport.domain;
-
-import lombok.Setter;
-import lombok.Getter;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import java.util.HashSet;
-import java.util.Set;
-
-/**
- * Created by rapa on 18/05/18.
- */
-@Getter
-@Setter
-@Entity
-public class Course {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-    private String name;
-    private String description;
-
-    @OneToMany(mappedBy = "course")
-    private Set<Lesson> lessons = new HashSet<>();
-
-}
+//package com.eatstime.eatstimesupport.domain;
+//
+//import lombok.Setter;
+//import lombok.Getter;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.Id;
+//import javax.persistence.OneToMany;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+///**
+// * Created by rapa on 18/05/18.
+// */
+//@Getter
+//@Setter
+//@Entity
+//public class Course {
+//
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+//    private String name;
+//    private String description;
+//
+//    @OneToMany(mappedBy = "course")
+//    private Set<Lesson> lessons = new HashSet<>();
+//
+//}
--- src/main/java/com/eatstime/eatstimesupport/domain/User.java
+++ src/main/java/com/eatstime/eatstimesupport/domain/User.java
@@ -1,25 +1,18 @@
 package com.eatstime.eatstimesupport.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 
 /**
  * Created by rapa on 20/05/18.
  */
 @Getter
 @Setter
-@Entity
 @NoArgsConstructor
 public class User {
-    @Id
-    @GeneratedValue
-    private Long id;
+
 
     private String name;
     private String email;
